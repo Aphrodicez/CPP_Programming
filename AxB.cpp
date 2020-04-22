cdb30408aea9c6c80d7952bf996c217c1a9e4e54
Kang.Chayanin <57552210+Aphrodicez@users.noreply.github.com>
2020-04-22 11:02:37 +0700

    Add files via upload

---
+++ AxB.cpp
@@ -0,0 +1,17 @@
+/*
+    TASK:
+    LANG: CPP
+    AUTHOR: Aphrodicez
+    SCHOOL: RYW
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a,b;
+    scanf("%lld %lld",&a,&b);
+    printf("%lld",a*b);
+    return 0;
+}
