7d449d99ded4b02a39d20b798a1b80f85c6d314e
KangChayanin <chayanin15632@gmail.com>
2020-05-13 07:45:28 +0700

    Pre_TOI (#1)

--- CodeCube.in.th/Pre_TOI/Pre_TOI_16 (#1)/Humanity_Has_Declined.cpp
+++ CodeCube.in.th/Pre_TOI/Pre_TOI_16 (#1)/Humanity_Has_Declined.cpp
@@ -1,5 +1,5 @@
 /*
-    TASK:
+    TASK: Humanity Has Declined
     LANG: CPP
     AUTHOR: Aphrodicez
     SCHOOL: RYW
@@ -13,23 +13,19 @@ int count_Number[200010];
 int right_Border[200010];
 
 void solve(int n, int k) {
-
     int l = n, r = n, cnt = 0, i;
     while (l > 0) {
-
         if (a[l] <= k) {
             if (!count_Number[a[l]])
                 cnt++;
             count_Number[a[l]]++;
         }
-
         while (cnt == k) {
             if (a[r] <= k && count_Number[a[r]] == 1)
                 break;
             count_Number[a[r]]--;
             r--;
         }
-
         if (cnt == k)
             right_Border[l] = r;
         else
@@ -41,23 +37,15 @@ void solve(int n, int k) {
 int main()
 {
     int n, k, q, l, r;
-
     int i, j;
-
     scanf("%d %d %d", &n, &k, &q);
-
     for (i = 1; i <= n; i++)
         scanf("%d", &a[i]);
-
     solve(n, k);
-
     while (q--) {
-
         scanf("%d %d", &l, &r);
-
         if (right_Border[l] > r)
             printf("NO\n");
-
         else
             printf("YES\n");
     }
