d53eddd8fb59ff886c01975542867c58fab1f67b
KangChayanin <chayanin15632@gmail.com>
2020-05-13 09:06:37 +0700

    Easy

---
+++ CodeCube.in.th/Easy/I_like_CodeCube.cpp
@@ -0,0 +1,18 @@
+/*
+    TASK: I like CodeCube
+    LANG: CPP
+    AUTHOR: Aphrodicez
+    SCHOOL: RYW
+*/
+
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        printf("CodeCube\n");
+    return 0;
+}
---
+++ CodeCube.in.th/Easy/MaxMin.cpp
@@ -0,0 +1,24 @@
+/*
+    TASK: MaxMin
+    LANG: CPP
+    AUTHOR: Aphrodicez
+    SCHOOL: RYW
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i, minn = 2e9, maxx = -2e9, n, num;
+    scanf("%d", &n);
+    scanf("%d", &num);
+    minn = num, maxx = num;
+    for (i = 1; i < n; i++) {
+        scanf("%d", &num);
+        if (maxx < num) maxx = num;
+        if (minn > num) minn = num;
+    }
+    printf("%d\n%d", maxx, minn);
+    return 0;
+}
